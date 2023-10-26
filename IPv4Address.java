3047449c9c5767d6d4140fd4ae412e808e16299e
Patric Pintescul <patric.personal99@gmail.com>
2023-10-26 13:07:46 +0200

    added comments

--- IPv4Address.java
+++ IPv4Address.java
@@ -1,24 +1,36 @@
 import java.util.regex.Pattern;
 
 /**
- * IPv4Address
+ * Represents an IPv4 address and provides methods to validate different types of IPv4 addresses.
  */
 public class IPv4Address {
-
+    /**
+     * Regular expression pattern for validating a global IPv4 address.
+     */
     private static final Pattern IP_PATTERN = Pattern.compile("^(?!127\\.0\\.0\\.1)(([0-9]|[1-9][0-9]|1([0-9]{2})|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1([0-9]{2})|25[0-5])$");
+    /**
+     * Regular expression pattern for validating a local IPv4 address.
+     */
     private static final Pattern LOCAL_IP_PATTERN = Pattern.compile("192\\.168\\.([0-9]|[1-9][0-9]|1([0-9]{2})|25[0-5])\\.([0-9]|[1-9][0-9]|1([0-9]{2})|25[0-5])$");
+    /**
+     * Constant representing a local IP address.
+     */
     public static final int LOCAL_IP = 1;
-    public static final int GLOBAL_IP = 0;
 
+    /**
+     * Constant representing a global IP address.
+     */
+    public static final int GLOBAL_IP = 0;
     private String addr;
     private int addrType;
 
-    public int getAddrType() {
-        return addrType;
-    }
-    public void setAddrType(int addrType) {
-        this.addrType = addrType;
-    }
+    /**
+     * Constructs an IPv4Address object with the given address and address type.
+     *
+     * @param addr     The IPv4 address as a String.
+     * @param addrType The type of the address: LOCAL_IP or GLOBAL_IP.
+     * @throws IllegalArgumentException If the provided address is invalid.
+     */
     public IPv4Address(String addr,int addrType) {
         if(addrType==0)
         if (!IP_PATTERN.matcher(addr).matches()) {
@@ -32,16 +44,32 @@ public class IPv4Address {
         this.addrType = addrType;
         this.addr = addr;
     }
+
+    /**
+     * Default constructor initializing the IPv4Address object with null address and
+     * undefined address type.
+     */
     public IPv4Address(){
         this.addr=null;
         this.addrType=-1;
     }
 
+
+    /**
+     * Gets the IPv4 address.
+     *
+     * @return The IPv4 address as a String.
+     */
     public String getAddr() {
         return addr;
     }
 
-    public void setAddr(String addr) {
-        this.addr = addr;
+    /**
+     * Gets the type of the IPv4 address.
+     *
+     * @return The address type: LOCAL_IP or GLOBAL_IP.
+     */
+    public int getAddrType() {
+        return addrType;
     }
 }
--- MainApp.java
+++ MainApp.java
@@ -1,14 +1,27 @@
 import java.util.Scanner;
 
 /**
- * MainApp
+ * This is the main class of the application.
+ * It instantiates an IPv4Address object, a class that manages IPv4 addresses, including local addresses.
  */
 public class MainApp {
+    /**
+     * The entry point of the application.
+     * It prompts the user to enter an IPv4 address and its type (local or global),
+     * validates the input, and displays the validated IP address along with its
+     * type.
+     *
+     * @param args The command line arguments (not used in this application).
+     */
     public static void main(String[] args) {
+        // Instantiate an IPv4Address object
         IPv4Address addr=new IPv4Address();
 
+        // Create a Scanner object to read user input
         Scanner sc = new Scanner(System.in);
         int addrType=-1;
+        // Prompt the user for address type until a valid input (0 for IPv4, 1 for IPv6)
+        // is provided
         while(addrType<0||addrType>1){
             try {
                 System.out.println("Enter 0 for IPv4 address or 1 for IPv6 address");
@@ -17,16 +30,22 @@ public class MainApp {
                 System.out.println("Invalid selection");
             }
         }
+        // Prompt the user for an IPv4 address until a valid address is provided
         while(addr.getAddr()==null){
             String ip = sc.nextLine();
             try{
+                // Attempt to create an IPv4Address object with the user-provided address and
+                // type
                 addr = new IPv4Address(ip,addrType);
             }catch (IllegalArgumentException e){
                 System.out.println("Invalid IPv4 address");
             }
         }
-        System.out.println("Valid IP Address of type"+(addr.getAddrType()==0 ? "Global" : "Local"));
+        // Display the validated IP address and its type (local or global)
+        System.out.println("Valid IP Address of type " + (addr.getAddrType() == 0 ? "Global" : "Local"));
         System.out.println(addr.getAddr());
+
+        // Close the Scanner object to prevent resource leaks
         sc.close();
     }
 }
